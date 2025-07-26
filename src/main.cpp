db50de49aa0b96c7b7b10f4b6fd6ba6b2a8f9580
irp <95143502+IanRPage@users.noreply.github.com>
2025-07-26 14:07:22 -0400

    Added Graph and SFML (#2)
    * added sfml to project
    
    * created templated Graph structure
    
    i created a base for our Graph structure. using an adjacency matrix for
    representation since graph is dense.
    
    also altered CMakeLists.txt a little bit, along with directory structure
    
    * defined new directory structure
    
    * changed directory structure
    
    i changed the directory structure to help with organization and compile
    times. changing something in the ui, dsa, or game libraries won't cause
    the whole project to recompile, just what was changed.
    
    added a test function in window.h, defined it in window.cpp. the test
    function uses SFML to create a window and draw a circle into it. then i
    bring it into main.cpp to draw

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
+#include <dsa/Graph.h>
+#include <ui/window.h>
 #include <iostream>
 
-using namespace std;
+int main() {
+	Graph<int> testing(10, 10);
 
-int main(){
-	cout << "Hello AlgoSnake!\n";
-	return 0;
+	std::vector<std::pair<int, int>> neighbors = testing.getNodeNeighbors(4, 4);
+	std::cout << "Neighbors of (4, 4):\n";
+	for (const auto &pair : neighbors) {
+		std::cout << "(" << pair.first << ", " << pair.second << ")\n";
+	}
+
+	neighbors = testing.getNodeNeighbors(0, 0);
+	std::cout << "Neighbors of (0, 0):\n";
+	for (const auto &pair : neighbors) {
+		std::cout << "(" << pair.first << ", " << pair.second << ")\n";
+	}
+
+	return test();
 }
